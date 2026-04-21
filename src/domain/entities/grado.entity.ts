00e547634c39107848ca3f0b904f7a0584f42cbe
Juan Villar <henryth27x@gmail.com>
2026-04-21 18:55:06 -0500

    feat: agregando entidades basicas

---
+++ src/domain/entities/grado.entity.ts
@@ -0,0 +1,24 @@
+import { CustomError } from "./custom.error";
+
+export class GradoEntity {
+    constructor(
+        public readonly id: number,
+        public nivel: "inicial" | "primaria" | "secundaria",
+        public year: number,
+    ) {}
+
+    static fromObject(obj: any): GradoEntity {
+        if (!obj.id) 
+            throw CustomError.badRequest("Id requerido");
+        if (!["inicial","primaria","secundaria"].includes(obj.nivel))
+            throw CustomError.unprocessable("Nivel inválido");
+        if (!obj.year || isNaN(Number(obj.year)))
+            throw CustomError.badRequest("Año inválido");
+
+        return new GradoEntity(
+            obj.id,
+            obj.nivel,
+            Number(obj.year),
+        );
+    }
+}
